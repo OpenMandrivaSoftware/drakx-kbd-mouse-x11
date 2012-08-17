783845505e0a6a22a6afe709408120e30c9accaf
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-08-17 08:25:25 +0000

    From: colin <colin@5b7c3c08-40e5-403b-9995-ace06908e4af> Date: Sun, 18 Dec 2011 20:57:14 +0000 Subject: [PATCH 060/116] Convert mouse helper to use udevadm in order to work  with udev 175+
    git-svn-id: svn://svn.mageia.org/soft/drakx-kbd-mouse-x11/trunk@2477 5b7c3c08-40e5-403b-9995-ace06908e4af
    
    
    
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx-kbd-mouse-x11/trunk@274834 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- lib/mouse.pm
+++ lib/mouse.pm
@@ -193,8 +193,14 @@ sub write {
 sub _input_entry_to_device_by_id {
     my ($input) = @_;
 
-    my $ID_SERIAL = chomp_(run_program::get_stdout('usb_id', $input->{sysfs_path}));
-    $ID_SERIAL =~ s/[^\w#+\-.:=@_]/_/g; #- udev do a further cleanup, eg: "Wacom_Co.,Ltd._MTE-450" => "Wacom_Co._Ltd._MTE-450". cf ALLOWED_CHARS udev.h
+    my $ID_SERIAL = "";
+    for (split /^/,  run_program::get_stdout('udevadm', 'info', '--query', 'env', '--path', $input->{sysfs_path})) {
+      if (/^ID_SERIAL=(.*)/) {
+        $ID_SERIAL = $1;
+        $ID_SERIAL =~ s/[^\w#+\-.:=@_]/_/g; #- udev do a further cleanup, eg: "Wacom_Co.,Ltd._MTE-450" => "Wacom_Co._Ltd._MTE-450". cf ALLOWED_CHARS udev.h
+        break;
+      }
+    }
     my $sysfs_device = "input/by-id/usb-$ID_SERIAL-event-mouse"; #- from /etc/udev/rules.d/60-persistent-input.rules
     if ($::isInstall || -e "/dev/$sysfs_device") {
 	$sysfs_device;
