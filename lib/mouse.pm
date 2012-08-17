0817adf1cdc0d4722051420b4f95b4448fad58ab
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-08-17 08:27:05 +0000

    From: tv <tv@5b7c3c08-40e5-403b-9995-ace06908e4af> Date: Mon, 19 Dec 2011 09:55:21 +0000 Subject: [PATCH 065/116] cleanups
    git-svn-id: svn://svn.mageia.org/soft/drakx-kbd-mouse-x11/trunk@2485 5b7c3c08-40e5-403b-9995-ace06908e4af
    
    
    
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx-kbd-mouse-x11/trunk@274838 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- lib/mouse.pm
+++ lib/mouse.pm
@@ -197,11 +197,11 @@ sub _input_entry_to_device_by_id {
     my ($input) = @_;
 
     my $ID_SERIAL = "";
-    for (split /^/,  run_program::get_stdout('udevadm', 'info', '--query', 'env', '--path', $input->{sysfs_path})) {
+    foreach (split /^/,  run_program::get_stdout('udevadm', 'info', '--query', 'env', '--path', $input->{sysfs_path})) {
       if (/^ID_SERIAL=(.*)/) {
         $ID_SERIAL = $1;
         $ID_SERIAL =~ s/[^\w#+\-.:=@_]/_/g; #- udev do a further cleanup, eg: "Wacom_Co.,Ltd._MTE-450" => "Wacom_Co._Ltd._MTE-450". cf ALLOWED_CHARS udev.h
-        break;
+        last;
       }
     }
     my $sysfs_device = "input/by-id/usb-$ID_SERIAL-event-mouse"; #- from /etc/udev/rules.d/60-persistent-input.rules
