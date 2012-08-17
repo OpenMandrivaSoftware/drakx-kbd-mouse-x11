OpenMandrivaSoftware/drakx-kbd-mouse-x11

commit 0bf34c89f77d5029d231965cd96578a172bbb5a5

From: anssi <anssi@5b7c3c08-40e5-403b-9995-ace06908e4af> Date: Thu, 5 Apr 2012 02:49:52 +0000 Subject: [PATCH 090/116] Fix firmware handling (it is DRIVER which needs it,  not DRIVER2)
git-svn-id: svn://svn.mageia.org/soft/drakx-kbd-mouse-x11/trunk@3925 5b7c3c08-40e5-403b-9995-ace06908e4af



git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx-kbd-mouse-x11/trunk@274863 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

lib/Xconfig/card.pm
@@ -311,7 +311,6 @@ sub install_server {
     my ($card, $options, $do_pkgs, $o_in) = @_;
 
     my @packages;
-    my @must_have = "x11-driver-video-$card->{Driver}";
 
     if ($options->{freedriver}) {
 	delete $card->{Driver2};
@@ -320,7 +319,6 @@ sub install_server {
     if ($card->{Driver2}) {       
 	require Xconfig::proprietary;
 	Xconfig::proprietary::handle_DRIVER2_NO_SSE($card);
-	Xconfig::proprietary::handle_FIRMWARE($do_pkgs, $card);
 	my @pkgs = Xconfig::proprietary::pkgs_for_Driver2($card->{Driver2}, $do_pkgs);
 	if (@pkgs && (!$o_in || $o_in->ask_yesorno('', formatAlaTeX(N("There is a proprietary driver available for your video card which may support additional features.
 Do you wish to use it?")), 1))) {
@@ -329,6 +327,11 @@ Do you wish to use it?")), 1))) {
 	    delete $card->{Driver2};
 	}
     }
+
+    Xconfig::proprietary::handle_FIRMWARE($do_pkgs, $card);
+
+    # handle_FIRMWARE could've changed $card->{Driver}
+    my @must_have = "x11-driver-video-$card->{Driver}";
 
     $do_pkgs->ensure_are_installed([ @must_have, @packages ], 1) or
       @must_have == listlength($do_pkgs->are_installed(@must_have))
@@ -496,10 +499,10 @@ sub readCardsDB {
         LINE => sub { $val =~ s/^\s*//; $card->{raw_LINES} .= "$val\n" },
 	CHIPSET => sub { $card->{Chipset} = $val },
 	DRIVER => sub { $card->{Driver} = $val },
+	DRIVER_NO_FIRMWARE => sub { $card->{DRIVER_NO_FIRMWARE} = $val },
 	DRIVER2 => sub { $card->{Driver2} = $val },
 	DRIVER2_NEEDS_SSE => sub { $card->{DRIVER2_NEEDS_SSE} = 1 },
 	DRIVER2_NO_SSE => sub { $card->{DRIVER2_NO_SSE} = $val },
-	DRIVER2_NO_FIRMWARE => sub { $card->{DRIVER2_NO_FIRMWARE} = $val },
 	FIRMWARE => sub { $card->{FIRMWARE} = $val },
 	NEEDVIDEORAM => sub { $card->{needVideoRam} = 1 },
 	DRI_GLX => sub { $card->{DRI_GLX} = 1 if $card->{Driver} },

lib/Xconfig/proprietary.pm
@@ -53,12 +53,12 @@ sub handle_FIRMWARE {
     my $pkg = $card->{FIRMWARE} or return;
 
     $do_pkgs->is_installed($pkg) || $do_pkgs->install($pkg) and return;
+    if ($card->{DRIVER_NO_FIRMWARE}) {
+	log::l("$card->{Driver} need a firmware to work, switching back to $card->{DRIVER_NO_FIRMWARE}");
+	$card->{Driver} = $card->{DRIVER_NO_FIRMWARE};
 
-    if ($card->{DRIVER2_NO_FIRMWARE}) {
-	log::l("$card->{Driver2} need a firmware to work, switching back to $card->{DRIVER2_NO_FIRMWARE}");
-	$card->{Driver2} = $card->{DRIVER2_NO_FIRMWARE};
     } else {
-	log::l("$card->{Driver2} needs a firmware to work smoothly/better (eg: 3D, KMS) but will still work");
+	log::l("$card->{Driver} needs a firmware to work smoothly/better (eg: 3D, KMS) but will still work");
     }
 }
 

commit a6646f579abca9780885f9a738986e11d0595b94

From: tv <tv@5b7c3c08-40e5-403b-9995-ace06908e4af> Date: Thu, 5 Apr 2012 17:42:02 +0000 Subject: [PATCH 100/116] (install_server) fix loading functions
thus fixing crash introduced by anssi in r3925
["Fix firmware handling (it is DRIVER which needs it, not DRIVER2)"]

git-svn-id: svn://svn.mageia.org/soft/drakx-kbd-mouse-x11/trunk@3957 5b7c3c08-40e5-403b-9995-ace06908e4af



git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/drakx-kbd-mouse-x11/trunk@274872 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

lib/Xconfig/card.pm
@@ -316,8 +316,8 @@ sub install_server {
 	delete $card->{Driver2};
     }
 
+    require Xconfig::proprietary;
     if ($card->{Driver2}) {       
-	require Xconfig::proprietary;
 	Xconfig::proprietary::handle_DRIVER2_NO_SSE($card);
 	my @pkgs = Xconfig::proprietary::pkgs_for_Driver2($card->{Driver2}, $do_pkgs);
 	if (@pkgs && (!$o_in || $o_in->ask_yesorno('', formatAlaTeX(N("There is a proprietary driver available for your video card which may support additional features.