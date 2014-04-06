999c6f9de0bf2638d366cda383fac1cce8544cad
Crispin Boylan <crisb@mandriva.org>
2014-04-06 09:18:32 +0100

    Fix typo

--- lib/Xconfig/various.pm
+++ lib/Xconfig/various.pm
@@ -238,7 +238,7 @@ sub runlevel {
 	}
     } else {
 	cat_($f) =~ /^id:(\d):initdefault:\s*$/m && return $1;
-	my $link = readlink("$::prefix/etc/systemd/system/default.target")
+	my $link = readlink("$::prefix/etc/systemd/system/default.target");
         $link =~ /runlevel(\d).target/m && return $1;
         $link =~ /graphical.target/m && return 5;
     }
